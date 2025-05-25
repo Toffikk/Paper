947eb9d67b8a6a61fa987854afb38ea37d8b700b
Toffikk <slodkitofik@gmail.com>
2025-05-25 14:23:21 +0200

    some more changes

--- paper-server/src/main/java/com/destroystokyo/paper/PaperVersionFetcher.java
+++ paper-server/src/main/java/com/destroystokyo/paper/PaperVersionFetcher.java
@@ -97,17 +97,17 @@ public class PaperVersionFetcher implements VersionFetcher {
                 }
                 case DISTANCE_UNKNOWN -> SIMPLE_LOGGER.warn("*** You are running an unknown version! Cannot fetch version info ***");
                 case 1 -> {
-                    SIMPLE_LOGGER.error("*** You are " + "120" + " builds behind!");
+                    SIMPLE_LOGGER.error("*** You are " + "120" + " builds behind! ***");
                     SIMPLE_LOGGER.error("*** Please download a new build from " + DOWNLOAD_PAGE + " ***");
                     if (newVersionAvailable) SIMPLE_LOGGER.error("*** Also note that a new Minecraft version has been released (" + newVersion + ")! ***");
                 }
                 default -> {
                     if (newVersionAvailable) {
-                        SIMPLE_LOGGER.error("*** Currently you are " + "120" + " build(s) behind");
+                        SIMPLE_LOGGER.error("*** Currently you are " + "120" + " build(s) behind ***");
                         SIMPLE_LOGGER.error("*** It is highly recommended to download a new build from " + DOWNLOAD_PAGE + " ***");
                         SIMPLE_LOGGER.error("*** Also note that a new Minecraft version has been released (" + newVersion + ")! ***");
                     } else {
-                        SIMPLE_LOGGER.warn("*** Currently you are " + distance + " build(s) behind");
+                        SIMPLE_LOGGER.warn("*** Currently you are " + distance + " build(s) behind ***");
                         SIMPLE_LOGGER.warn("*** It is highly recommended to download a new build from " + DOWNLOAD_PAGE + " ***");
                     }
                 }
